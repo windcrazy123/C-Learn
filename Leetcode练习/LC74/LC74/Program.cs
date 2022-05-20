12583172d971d171a94c4b22032fa7bf3857b3ed
WindCrazy <82226647+windcrazy123@users.noreply.github.com>
2022-05-20 22:18:08 +0800

    Add files via upload

---
+++ Leetcode练习/LC74/LC74/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace LC74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool output;
+            int[][] matrix = new int[0][];
+            //matrix[0] = new int[1] { 1 };
+
+            Solution solution = new Solution();
+            output = solution.SearchMatrix(matrix, 0);
+            Console.WriteLine(output);
+        }
+    }
+    public class Solution
+    {
+        //没有考虑到matrix == null || matrix.length == 0的情况
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            bool key = false;
+            int up = 0;
+            int left = 0;
+            int m = matrix.Length - 1;
+            int n = matrix[0].Length - 1;
+            while (up<=m)
+            {
+                int mid = up + (m - up) / 2;//可简化为:int mid = up + (m - up) >> 1 右移一位
+                if (matrix[mid][0]==target)
+                {
+                    return !key;
+                }else if (matrix[mid][0] < target)
+                {
+                    up = mid + 1;
+                }
+                else
+                {
+                    m = mid - 1;
+                }
+            }
+            while (left<=n)
+            {
+                try
+                {
+                    int mi = left + (n - left) / 2;
+                    if (matrix[m][mi]==target)
+                    {
+                        return !key;
+                    }else if (matrix[m][mi] < target)
+                    {
+                        left = mi + 1;
+                    }
+                    else
+                    {
+                        n = mi - 1;
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+                
+            }
+            return key;
+        }
+    }
+}
+//以下方法是将我的遗漏补充，且
+//将两个while循环合并但比我的方法慢将近两倍，且
+/*
+ public boolean searchMatrix(int[][] matrix, int target) {
+    if (matrix == null || matrix.length == 0) {
+        return false;
+    }
+    int start = 0, rows = matrix.length, cols = matrix[0].length;
+    int end = rows * cols - 1;
+    while (start <= end) {
+        int mid = (start + end) / 2;//不如我的方法好，因为start+end可能容易溢出
+        if (matrix[mid / cols][mid % cols] == target) {
+            return true;
+        }
+        if (matrix[mid / cols][mid % cols] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return false;
+}
+ */
---
+++ Leetcode练习/LC744/LC744/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace LC744
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char output;
+            char[] array = { 'a', 'b', 'c', 'e', 'f' };
+            Seach seach = new Seach();
+            output = seach.BinarySeach(array, 'd');
+            Console.WriteLine(output);
+        }
+    }
+    class Seach
+    {
+        public char BinarySeach(char[] letters, char target)
+        {
+            int min = 0;
+            int max = letters.Length - 1;
+            int left = 0;
+            int right = letters.Length - 1;
+            if ((letters[min] > target) || (letters[max] <= target))
+            {
+                return letters[min];
+            }
+            else {
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if ((letters[mid] <= target) && (letters[mid + 1] > target))
+                    {
+                        return letters[mid + 1];
+                    } else if (target < letters[mid])
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+            }
+            return '!';
+        }
+    }
+    /*
+     public class Solution {
+        public char NextGreatestLetter(char[] A, char target) {
+            int low=0,high=A.Length-1;
+            char R='-'; 
+            while(low<=high)
+            {
+                int mid=low+(high-low)/2;
+                if(A[mid] > target)
+                {
+                    high=mid-1;
+                    R=A[mid];
+                }
+                else
+                {
+                    low=mid+1;
+                }   
+            }
+            return R=='-'?A[0]:R;
+        }
+    }
+     */
+    /*public class Solution
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            int lo = 0;
+            int hi = letters.Length - 1;
+            int targetIndex = -1;
+            while (lo <= hi)
+            {
+                int mi = lo + (hi - lo) / 2;
+                // If current is smaller or equal than target
+                // Keep this index and continue search in right part
+                if (letters[mi] <= target)
+                {
+                    targetIndex = mi;
+                    lo = mi + 1;
+                }
+                else
+                {
+                    hi = mi - 1;
+                }
+            }
+
+            int next = (targetIndex + 1) % letters.Length;
+            return letters[next];
+        }
+    }*/
+}
